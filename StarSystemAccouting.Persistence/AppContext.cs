54be92ac8e39c791abb6aaadf706826a0ab14b89
Maksim <59286805+Maflend@users.noreply.github.com>
2022-03-10 14:18:34 +0300

    Add AppContext and DependecyInjection in Persistence.

---
+++ StarSystemAccouting.Persistence/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StarSystemAccouting.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarSystemAccouting.Persistence
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+        public DbSet<SpaceObject> SpaceObjects { get; set; }
+        public DbSet<StarSystem> StarSystems { get; set; }
+    }
+}
