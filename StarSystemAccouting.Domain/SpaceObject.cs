74da0d838474f709839f4bb7d8578b03e7be5d78
Maksim <59286805+Maflend@users.noreply.github.com>
2022-03-10 13:25:20 +0300

    Add SpaceObject, StarSystem  entities

---
+++ StarSystemAccouting.Domain/SpaceObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarSystemAccouting.Domain
+{
+    public class SpaceObject
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Age { get; set; }
+        public double Diameter { get; set; }
+        public double Weight { get; set; }
+        public StarSystem StarSystem { get; set; }
+    }
+}
---
+++ StarSystemAccouting.Domain/StarSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarSystemAccouting.Domain
+{
+    public class StarSystem
+    {
+        public string Name { get; set; }
+        public double Age { get; set; }
+        public SpaceObject CenterOfGravity { get; set; }
+        public List<SpaceObject> SpaceObjects { get; set; }
+    }
+}
