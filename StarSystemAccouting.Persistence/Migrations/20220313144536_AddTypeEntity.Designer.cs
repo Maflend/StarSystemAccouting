08264e2b292bc8c3e3c9e081f858d109658f353e
Maksim <59286805+Maflend@users.noreply.github.com>
2022-03-13 17:46:52 +0300

    create migration

--- StarSystemAccouting.Persistence/Migrations/20220313144536_AddTypeEntity.Designer.cs
+++ StarSystemAccouting.Persistence/Migrations/20220313144536_AddTypeEntity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using StarSystemAccouting.Persistence;
@@ -11,9 +12,10 @@ using StarSystemAccouting.Persistence;
 namespace StarSystemAccouting.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20220313144536_AddTypeEntity")]
+    partial class AddTypeEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ StarSystemAccouting.Persistence/Migrations/20220313144536_AddTypeEntity.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StarSystemAccouting.Persistence.Migrations
+{
+    public partial class AddTypeEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ObjectType",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ObjectType", x => x.Name);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpaceObjects_Type",
+                table: "SpaceObjects",
+                column: "Type");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ObjectType_Name",
+                table: "ObjectType",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SpaceObjects_ObjectType_Type",
+                table: "SpaceObjects",
+                column: "Type",
+                principalTable: "ObjectType",
+                principalColumn: "Name",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SpaceObjects_ObjectType_Type",
+                table: "SpaceObjects");
+
+            migrationBuilder.DropTable(
+                name: "ObjectType");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SpaceObjects_Type",
+                table: "SpaceObjects");
+        }
+    }
+}
