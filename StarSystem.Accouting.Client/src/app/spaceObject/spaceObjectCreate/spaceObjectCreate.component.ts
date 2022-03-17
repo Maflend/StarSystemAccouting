7f24cf7f81624371121c1983d39b5394f0d9efc1
Maksim <59286805+Maflend@users.noreply.github.com>
2022-03-17 15:31:38 +0300

    working create space object without http err handl

---
+++ StarSystem.Accouting.Client/src/app/spaceObject/spaceObjectCreate/spaceObjectCreate.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core'
+import {SpaceObjectService} from '../../services/spaceObject.service'
+import {SpaceObjectCreate} from '../spaceObjectCreate.model';
+import {SpaceObjectCreateRequest} from '../spaceObjectCreateRequest.model';
+import {StarSystem} from '../../starSystem/starSystem.model'
+import { Guid } from "guid-typescript";
+import { NgForm } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+
+@Component({
+    selector:'spaceObjectCreate-comp',
+    templateUrl: './spaceObjectCreate.html',
+    providers:[SpaceObjectService]
+})
+
+export class SpaceObjectCreateComponent implements OnInit{
+    spaceObject: SpaceObjectCreate = new SpaceObjectCreate();
+    starSystem: StarSystem[] = [];
+    
+    constructor(private spaceObjectService: SpaceObjectService){}
+    id: Guid = Guid.createEmpty();
+    submitt(myForm: NgForm){
+        console.log(myForm);
+    }
+    submit(myForm: NgForm){
+        
+        this.id = this.starSystem.filter(s=>s.name == myForm.value.starSystemName)[0].id;
+        this.spaceObjectService.create(new SpaceObjectCreateRequest( myForm.value.name,
+            myForm.value.type, myForm.value.age, myForm.value.diameter, myForm.value.weight, this.id));
+    
+    }
+   
+
+    ngOnInit(){
+      this.spaceObjectService.getStarSystems().subscribe((data:any)=>this.starSystem = data);
+    }
+}
